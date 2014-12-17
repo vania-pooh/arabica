d698227d2773bffc642c37c860d095eb4f6612b8
Ivan Krutov <vania-pooh@vania-pooh.com>
2014-12-17 23:05:59 +0300

    Added first launcher implementation

--- arabica-actions/src/main/java/org/meridor/arabica/impl/LauncherImpl.java
+++ arabica-actions/src/main/java/org/meridor/arabica/impl/LauncherImpl.java
@@ -4,15 +4,37 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import org.meridor.arabica.Launcher;
 
+import java.util.concurrent.CountDownLatch;
+
 public class LauncherImpl implements Launcher {
+
+    private final CountDownLatch launcherThreadLatch = new CountDownLatch(1);
 
     @Override
     public <T extends Application> T launch(Class<T> applicationClass) throws Exception {
-        T instance = applicationClass.newInstance();
-        //TODO: this should actually launch application in a separate thread and return its instance
-        instance.init();
-        Stage stage = new Stage();
-        instance.start(stage);
-        return instance;
+        T applicationInstance = applicationClass.newInstance();
+        Thread launcherThread = getLauncherThread(applicationInstance);
+        launcherThread.start();
+        launcherThreadLatch.await();
+        return applicationInstance;
+    }
+
+    private <T extends Application> Thread getLauncherThread(T applicationInstance){
+        return new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                //TODO: this should actually launch application in a separate thread and return its instance
+                try {
+                    applicationInstance.init();
+                    Stage stage = new Stage();
+                    applicationInstance.start(stage);
+                } catch (Exception e) {
+                    throw new RuntimeException(e); //TODO: replace by something better
+                } finally {
+                    launcherThreadLatch.countDown();
+                }
+            }
+        };
     }
 }
